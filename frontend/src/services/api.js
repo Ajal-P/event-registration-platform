25f52f90d4115b9283351f82e79ffebf1f5d9927
Ajal-P <ajalpramod06@gmail.com>
2026-06-16 13:32:13 +0530

    production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL:
+"https://event-registration-api.onrender.com/api/",  
 });
 
 api.interceptors.request.use((config) => {
