535b96d25962af6a1c317abc117ed7dc88a2dfd3
Ajal P <84313333+Ajal-P@users.noreply.github.com>
2026-06-16 16:01:34 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://event-registration-platform-zkg1.onrender.com/api/",
-});, 
+  baseURL: "https://event-registration-api.onrender.com/api/",
+});
 
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization =
-      `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
